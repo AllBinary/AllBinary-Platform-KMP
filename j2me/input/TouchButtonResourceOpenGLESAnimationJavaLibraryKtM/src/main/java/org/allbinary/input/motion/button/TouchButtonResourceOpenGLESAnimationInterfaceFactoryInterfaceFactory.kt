092a6c0e672ff81e416f692493ee3527c42fb35d
Travis Berthelot <travisberthelot@allbinary.com>
2025-08-04 00:38:56 -0500

    Update for KMP build

--- j2me/input/TouchButtonResourceOpenGLESAnimationJavaLibraryKtM/src/main/java/org/allbinary/input/motion/button/TouchButtonResourceOpenGLESAnimationInterfaceFactoryInterfaceFactory.kt
+++ j2me/input/TouchButtonResourceOpenGLESAnimationJavaLibraryKtM/src/main/java/org/allbinary/input/motion/button/TouchButtonResourceOpenGLESAnimationInterfaceFactoryInterfaceFactory.kt
@@ -44,8 +44,7 @@ public constructor        ()
 
 
                 @Throws(Exception::class)
-            
-open fun init(level: Int)
+            override fun init(level: Int)
         //nullable = true from not(false or (false and false)) = true
 {
 
@@ -53,8 +52,7 @@ open fun init(level: Int)
 super.init(OpenGLImageCacheFactory.getInstance(), level)
 }
 
-
-open fun isFeature()
+override fun isFeature()
         //nullable = true from not(false or (false and true)) = true
 : Boolean{
     
