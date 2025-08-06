1eaa71d2ade9a42774add8d86d0ca26bc2580b26
Travis Berthelot <travisberthelot@allbinary.com>
2025-08-06 01:26:20 -0500

    Updates for KMP Build

---
+++ graphics/opengl/OpenGLJ2MENoneJavaLibraryKtM/src/main/java/org/allbinary/device/NullGL10.kt
@@ -0,0 +1,44 @@
+
+        /* Generated Code Do Not Modify */
+        package org.allbinary.device
+
+
+
+        import java.lang.Integer
+        import java.lang.Math
+        import java.lang.Object
+        import java.lang.System
+        
+        
+        import kotlin.Array
+        import kotlin.reflect.KClass
+        
+import javax.microedition.khronos.opengles.GL10
+import java.nio.Buffer
+import java.nio.FloatBuffer
+import java.nio.IntBuffer
+import org.allbinary.logic.string.StringUtil
+
+open public class NullGL10
+            : Object
+        
+                , GL10 {
+        
+
+        companion object {
+
+
+    val NULL_GL10: NullGL10 = NullGL10()
+
+
+        }
+            
+            //Auto Generated
+            public constructor() : super()
+            {
+            }            
+        
+}
+                
+            
+
